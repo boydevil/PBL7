aade4249f30f8b326d7392955d8022bc5fd2a222
Boy Le <letrongsonghan@gmail.com>
2022-11-26 02:45:50 +0700

    server added

--- read.py
+++ read.py
@@ -3,16 +3,17 @@ import keras.applications.xception as xception
 from PIL import Image, ImageOps #Install pillow instead of PIL
 import numpy as np
 import warnings
+import requests
+from io import BytesIO
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model('model.h5', compile=False)
-model.summary()
+model = load_model('Models/model.h5', compile=False)
 # Load the labels
-class_names = open('labels.txt', 'r').readlines()
+class_names = open('Models/labels.txt', 'r').readlines()
 IMAGE_WIDTH = 71
 IMAGE_HEIGHT = 71
 # Create the array of the right shape to feed into the keras model
@@ -20,29 +21,31 @@ IMAGE_HEIGHT = 71
 # determined by the first position in the shape tuple, in this case 1.
 data = np.ndarray(shape=(1, IMAGE_WIDTH, IMAGE_HEIGHT, 3), dtype=np.float32)
 
-# Replace this with the path to your image
-image = Image.open('garbage_classification\\plastic\\plastic21.jpg').convert('RGB')
+def predict(USER_ID, IMG_ID):
+    # Replace this with the path to your image
+    BASE_URL = 'https://res.cloudinary.com/dj6tlm6my/image/upload/'
+    URL = BASE_URL + USER_ID + '/' +IMG_ID
+    response = requests.get(URL)
+    image = Image.open(BytesIO(response.content)).convert('RGB')
 
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (IMAGE_WIDTH, IMAGE_HEIGHT)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (IMAGE_WIDTH, IMAGE_HEIGHT)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-#turn the image into a numpy array
-image_array = np.asarray(image)
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32)) 
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32)) 
 
-# Load the image into the array
-data[0] = normalized_image_array
+    # Load the image into the array
+    data[0] = normalized_image_array
 
-# run the inference
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
+    # run the inference
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
 
-print('Class:', class_name, end='')
-print('Confidence score:', confidence_score)
-print(prediction[0])
+    return str(class_name) + ' ' + str(confidence_score)
